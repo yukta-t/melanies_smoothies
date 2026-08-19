3fe4dfc6119b255086a921e143ddb5e898f05196
yukta-t <yuktathakran@gmail.com>
2026-08-19 18:22:27 +0530

    Remove hardcoded watermelon API request
    Removed hardcoded API request for watermelon and its associated dataframe display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
+        sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)    
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
         values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -37,6 +40,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
